5198220ec7a540951ac7f66e47b2eabb635c301d
eluferiev <eluferiev@o-code.ru>
2022-06-17 15:25:58 +0400

    feat: calculate links

--- src/components/GraphView.vue
+++ src/components/GraphView.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="graph-view">
+    {{links}}
     <svg
       xmlns="http://www.w3.org/2000/svg"
       id="graph"
@@ -21,6 +22,48 @@ export default {
     return {
       svgContainer: null,
     };
+  },
+  computed: {
+    links() {
+      let arr = [];
+      if (this.doc.length > 0 && this.doc[0]['@graph']) {
+        const nodes = this.doc[0]['@graph'];
+        if (nodes.length > 0) {
+          nodes.forEach((link, index) => {
+            const source = {
+              id: link['@id'],
+              index,
+            };
+            Object.keys(link).forEach((key) => {
+              if (key !== '@id') {
+                let target;
+                if (key !== 'lego:hasAttribute') {
+                  target = {
+                    id: key,
+                    index: nodes.findIndex((el) => el['@id'] === key),
+                  }
+                  arr.push({ source, target, value: key, group: 'attribute' });
+                } else {
+                  link[key].forEach((attribute) => {
+                    const id = attribute['@id'];
+                    target = {
+                      id,
+                      index: nodes.findIndex((el) => el['@id'] === id),
+                    };
+                    arr.push({ source, target, value: id, group: 'main' });
+                  })
+                }
+              }
+            })
+          });
+        }
+      }
+      console.log(arr);
+      return arr;
+    },
+    nodes() {
+      return [];
+    },
   },
   created() {},
   mounted() {
